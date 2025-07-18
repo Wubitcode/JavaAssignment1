0cf85084be0c1b0d5f43ef811d4ece1b14c5978d
Wubitcode <wubit.eco@triosstudent.com>
2025-07-19 02:28:04 +0300

    Completed Question 2.14 – DisplayNumbers.java

---
+++ Question2_14/DisplayNumbers.java
@@ -0,0 +1,14 @@
+
+public class DisplayNumbers {
+    public static void main(String[] args) {
+
+        System.out.println("1 2 3 4");
+
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4\n");
+
+        System.out.printf("1 2 3 4\n");
+    }
+}
