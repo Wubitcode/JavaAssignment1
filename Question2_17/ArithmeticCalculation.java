84a2d53ea2924d0826550a6eea49b68971b4e849
Wubitcode <wubit.eco@triosstudent.com>
2025-07-19 04:31:36 +0300

    Add solution for Question 2.17 - Arithmetic Calculation

---
+++ Question2_17/ArithmeticCalculation.java
@@ -0,0 +1,11 @@
+public class ArithmeticCalculation {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        System.out.println("Sum: " + (number1 + number2));
+        System.out.println("Difference: " + (number1 - number2));
+        System.out.println("Product: " + (number1 * number2));
+        System.out.println("Quotient: " + (number1 / number2));
+    }
+}
