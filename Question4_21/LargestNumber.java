b3c0c7cbb5827ef0215fdc51274810aef1da2ad1
Wubitcode <wubit.eco@triosstudent.com>
2025-07-21 21:51:17 +0300

    Add solution for Question 4.21 - Largest of three integers

---
+++ Question4_21/LargestNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third integer: ");
+        int num3 = input.nextInt();
+
+        int largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        System.out.printf("The largest number is: %d%n", largest);
+    }
+}
